72824723079252cabf4bafbe5e51c81f7a4a52e4
andrew <and.ivchenkov@gmail.com>
2023-09-18 23:33:09 +0300

    test

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: 'eslint:recommended',
+  extends: ['eslint:recommended', 'plugin:vue/recommended'],
   rules: {
     'indent': ['error', 2],
     'linebreak-style': ['error', 'unix'],
@@ -7,5 +7,14 @@ module.exports = {
     'semi': ['error', 'never'],
     'comma-dangle': ['error', 'always-multiline'],
     'import/no-named-as-default-member': 'error',
+    'vue/order-in-components': 'error',
+    'vue/attribute-hyphenation': 'error',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/require-default-prop': 'error',
+    'vue/no-lone-template': 'error',
+    'vue/html-closing-bracket-spacing': ['error', { selfClosingTag: 'never' }],
   }
 }
