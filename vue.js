e699775c216438a03b29d1b034fc0ef1a333c9a9
andrew <and.ivchenkov@gmail.com>
2023-09-18 23:09:16 +0300

    separate vue rules

---
+++ vue.js
@@ -0,0 +1,13 @@
+module.exports = {
+  rules: {
+    'vue/order-in-components': 'error',
+    'vue/attribute-hyphenation': 'error',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/require-default-prop': 'error',
+    'vue/no-lone-template': 'error',
+    'vue/html-closing-bracket-spacing': ['error', { selfClosingTag: 'never' }],
+  }
+}
