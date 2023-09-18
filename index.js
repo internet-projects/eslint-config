6eaa6fc5f20ccb83a1371ef76b0386f3d3fb8d6c
andrew <and.ivchenkov@gmail.com>
2023-09-18 22:36:18 +0300

    init

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single', { 'avoidEscape': true }],
+    'semi': ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'import/no-named-as-default-member': 'error',
+    'vue/order-in-components': 'error',
+    'vue/no-v-html': 'off',
+    'vue/attribute-hyphenation': 'error',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/require-default-prop': 'error',
+    'vue/no-lone-template': 'error',
+    'vue/html-closing-bracket-spacing': ['error', { selfClosingTag: 'never' }],
+  }
+}
