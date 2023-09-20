10116d2e1d82cc9a44e7cd724a98d48790e54d11
andrew <and.ivchenkov@gmail.com>
2023-09-21 00:18:51 +0300

    switchCase

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:import/recommended'],
   rules: {
-    'indent': ['error', 2],
+    'indent': ['error', 2, { 'SwitchCase': 1 }],
     'linebreak-style': ['error', 'unix'],
     'quotes': ['error', 'single', { 'avoidEscape': true }],
     'semi': ['error', 'never'],
